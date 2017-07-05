2bf15f8547aa13fd8e5f2535dc5a3357db6795e3
madzathemes <madza@madzathemes.com>
2017-07-05 05:35:13 +0300

    fix

--- demos-themes-bar.php
+++ demos-themes-bar.php
@@ -27,8 +27,8 @@ function magazine_themes_bar() { ?>
         <a class="mtc-demo" href="http://nextnewsz.tophot.staging.wpengine.com/"><div><img src="http://nextnewsdemos.tophot.staging.wpengine.com/wp-content/uploads/sites/37/2017/07/demo_z.jpg"/></div></a>
       </div>
       <div>
-        <div class="mtc-demo mtc-soon"><div>Coming Soon</div></div>
-        <div class="mtc-demo mtc-soon"><div>Coming Soon</div></div>
+        <a class="mtc-demo" href="http://nextnewsz.tophot.staging.wpengine.com/"><div><img src="http://nextnewsdemos.tophot.staging.wpengine.com/wp-content/uploads/sites/37/2017/07/demo_z.jpg"/></div></a>
+        <a class="mtc-demo" href="http://nextnewsx.tophot.staging.wpengine.com/"><div><img src="http://nextnewsdemos.tophot.staging.wpengine.com/wp-content/uploads/sites/37/2017/07/demo_x.jpg"/></div></a>
         <div class="mtc-demo mtc-soon"><div>Coming Soon</div></div>
       </div>
     </div>
